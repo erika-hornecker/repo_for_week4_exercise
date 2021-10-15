e2c068f897a78d0f142a13ca280a8ff7c5270627
qianyans <2653507030@qq.com>
2021-10-15 13:05:02 -0400

    add function to create Fibonacci sequence

--- load_data.py
+++ load_data.py
@@ -2,3 +2,28 @@ import numpy as np
 import pandas as pd
 
 # In this file, you'll put all the functions for creating the Fibonacci sequence up to a certain number.
+
+def Fib_seq(nmax:float) -> list:
+    """
+    creating the Fibonacci sequence up to nmax
+
+    Parameters
+    ----------
+    nmax : float
+        upeer bound of the sequence
+
+    Returns
+    -------
+    list
+        Fibonacci sequence
+
+    """
+    
+    res = [0,1]
+    
+    i = 0
+    while res[i] + res[i+1] <= nmax:
+        res.append(res[i] + res[i+1])
+        i += 1
+        
+    return res
