90b4d39789560d9b9486936552cd0f1371b17734
Mairead Heiger <meh187@pitt.edu>
2021-10-21 09:17:39 -0400

    Add function that returns the fibonacci sequence

--- load_data.py
+++ load_data.py
@@ -2,3 +2,13 @@ import numpy as np
 import pandas as pd
 
 # In this file, you'll put all the functions for creating the Fibonacci sequence up to a certain number.
+
+def fibm(n):
+	fibm = []
+	def fib(n):
+		if n < 2:
+			return n
+		else:
+			return fib(n-1) + fib(n-2)
+	for i in range(n):
+		fibm.append(fib(i))
